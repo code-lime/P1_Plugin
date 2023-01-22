3b9d88d4e18126991538e98416314b8e90f6aca1
Lime <demn2001@gmail.com>
2023-01-23 02:47:57 +0300

    split `enchant remover` and `execute item`

--- src/main/java/org/lime/gp/item/Enchants.java
+++ src/main/java/org/lime/gp/item/Enchants.java
@@ -1,23 +1,13 @@
 package org.lime.gp.item;
 
 import com.google.gson.JsonArray;
-import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.enchantments.EnchantmentOffer;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
-import org.bukkit.event.entity.EntityPickupItemEvent;
-import org.bukkit.event.entity.EntitySpawnEvent;
-import org.bukkit.event.inventory.InventoryClickEvent;
-import org.bukkit.event.inventory.InventoryPickupItemEvent;
-import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.inventory.EntityEquipment;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.lime.core;
@@ -54,23 +44,10 @@ public class Enchants implements Listener {
         Enchants.allowEnchantments.clear();
         Enchants.allowEnchantments.addAll(allowEnchantments);
     }
-
-    @EventHandler public static void on(PrepareItemEnchantEvent e) {
-        EnchantmentOffer[] offers = e.getOffers();
-
-        int length = offers.length;
-        for (int i = 0; i < length; i++) {
-            EnchantmentOffer offer = offers[i];
-            if (offer == null || !allowEnchantments.contains(offer.getEnchantment())) continue;
-            offers[i] = null;
-        }
+    public static void init() {
+        ExecuteItem.execute.add(Enchants::onExecute);
     }
-
-    public static void replace(ItemStack original, ItemStack item) {
-        original.setType(item.getType());
-        original.setItemMeta(item.getItemMeta());
-        original.setAmount(item.getAmount());
-    }
+    
     private static boolean removeEnchants(system.Func0<Collection<Enchantment>> enchantments, system.Action1<Enchantment> remove, system.Action1<Enchantment> append, boolean not_empty) {
         boolean edit = false;
         for (Enchantment enchantment : enchantments.invoke()) {
@@ -84,54 +61,24 @@ public class Enchants implements Listener {
         }
         return edit;
     }
-    private static boolean executeItem(ItemStack item) {
-        if (item == null) return false;
-        ItemMeta meta = item.getItemMeta();
-        if (meta == null) return false;
-
-        ItemMeta new_meta = Items.getIDByItem(item).map(id -> {
-            Material mat = Items.creatorMaterials.getOrDefault(id, null);
-            if (mat != null && !item.getType().equals(mat) && Items.creators.getOrDefault(id, null) instanceof Items.ItemCreator creator && creator.updateReplace()) {
-                replace(item, creator.createItem(item.getAmount()));
-                return item.getItemMeta();
-            }
-            return null;
-        }).orElse(meta);
-
-        boolean save = new_meta != meta;
-        save = removeEnchants(() -> new_meta.getEnchants().keySet(), new_meta::removeEnchant, e -> new_meta.addEnchant(e, 1, true), false) || save;
+    public static Boolean onExecute(ItemStack item, system.Toast1<ItemMeta> metaBox) {
+        ItemMeta meta = metaBox.val0;
+        boolean save = removeEnchants(() -> meta.getEnchants().keySet(), meta::removeEnchant, e -> meta.addEnchant(e, 1, true), false);
         if (meta instanceof EnchantmentStorageMeta esm)
             save = removeEnchants(() -> esm.getStoredEnchants().keySet(), esm::removeStoredEnchant, e -> esm.addStoredEnchant(e, 1, true), true) || save;
 
-        if (save) {
-            item.setItemMeta(meta);
-            return true;
-        }
         return false;
     }
-    @EventHandler public static void on(InventoryClickEvent e) {
-        if (e.getWhoClicked().getInventory().equals(e.getClickedInventory())) {
-            executeItem(e.getCurrentItem());
-            executeItem(e.getCursor());
+
+    @EventHandler public static void on(PrepareItemEnchantEvent e) {
+        EnchantmentOffer[] offers = e.getOffers();
+
+        int length = offers.length;
+        for (int i = 0; i < length; i++) {
+            EnchantmentOffer offer = offers[i];
+            if (offer == null || !allowEnchantments.contains(offer.getEnchantment())) continue;
+            offers[i] = null;
         }
     }
-    @EventHandler public static void on(EntityPickupItemEvent e) { executeItem(e.getItem().getItemStack()); }
-    @EventHandler public static void on(InventoryPickupItemEvent e) { executeItem(e.getItem().getItemStack()); }
-    @EventHandler public static void on(PlayerDropItemEvent e) { executeItem(e.getItemDrop().getItemStack()); }
-    @EventHandler public static void on(EntitySpawnEvent e) {
-        if (!(e.getEntity() instanceof LivingEntity livingEntity)) return;
-        EntityEquipment equipment = livingEntity.getEquipment();
-        if (equipment == null) return;
-        ItemStack[] items = equipment.getArmorContents();
-        boolean save = false;
-        for (ItemStack item : items) save = executeItem(item) || save;
-        if (save) equipment.setArmorContents(items);
-    }
-    @EventHandler public static void on(PlayerJoinEvent e) {
-        PlayerInventory inventory = e.getPlayer().getInventory();
-        ItemStack[] items = inventory.getContents();
-        boolean save = false;
-        for (ItemStack item : items) save = executeItem(item) || save;
-        if (save) inventory.setContents(items);
-    }
+
 }
---
+++ src/main/java/org/lime/gp/item/ExecuteItem.java
@@ -0,0 +1,90 @@
+package org.lime.gp.item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryPickupItemEvent;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.lime.system;
+import org.lime.system.*;
+public class ExecuteItem implements Listener {
+    public static org.lime.core.element create() {
+        return org.lime.core.element.create(ExecuteItem.class)
+                .withInstance()
+                .withInit(ExecuteItem::init);
+    }
+    
+    public static final List<Func2<ItemStack, system.Toast1<ItemMeta>, Boolean>> execute = new ArrayList<>();
+
+    public static void init() {
+        execute.add(ExecuteItem::onExecute);
+    }
+    
+    public static void replace(ItemStack original, ItemStack item) {
+        original.setType(item.getType());
+        original.setItemMeta(item.getItemMeta());
+        original.setAmount(item.getAmount());
+    }
+
+    private static boolean onExecute(ItemStack item, system.Toast1<ItemMeta> metaBox) {
+        return Items.getIDByItem(item).map(id -> {
+            Material mat = Items.creatorMaterials.get(id);
+            if (mat == null || mat.equals(item.getType())) return false;
+            if (!(Items.creators.getOrDefault(id, null) instanceof Items.ItemCreator creator) || !creator.updateReplace()) return false;
+            replace(item, creator.createItem(item.getAmount()));
+            metaBox.val0 = item.getItemMeta();
+            return true;
+        }).orElse(false);
+    }
+
+    private static boolean executeItem(ItemStack item) {
+        if (item == null) return false;
+        ItemMeta meta = item.getItemMeta();
+        if (meta == null) return false;
+        system.Toast1<ItemMeta> metaBox = system.toast(meta);
+        boolean save = false;
+        for (Func2<ItemStack, system.Toast1<ItemMeta>, Boolean> func : execute)
+            save = func.invoke(item, metaBox) || save;
+        
+        if (!save) return false;
+        item.setItemMeta(metaBox.val0);
+        return true;
+    }
+    @EventHandler public static void on(InventoryClickEvent e) {
+        if (e.getWhoClicked().getInventory().equals(e.getClickedInventory())) {
+            executeItem(e.getCurrentItem());
+            executeItem(e.getCursor());
+        }
+    }
+    @EventHandler public static void on(EntityPickupItemEvent e) { executeItem(e.getItem().getItemStack()); }
+    @EventHandler public static void on(InventoryPickupItemEvent e) { executeItem(e.getItem().getItemStack()); }
+    @EventHandler public static void on(PlayerDropItemEvent e) { executeItem(e.getItemDrop().getItemStack()); }
+    @EventHandler public static void on(EntitySpawnEvent e) {
+        if (!(e.getEntity() instanceof LivingEntity livingEntity)) return;
+        EntityEquipment equipment = livingEntity.getEquipment();
+        if (equipment == null) return;
+        ItemStack[] items = equipment.getArmorContents();
+        boolean save = false;
+        for (ItemStack item : items) save = executeItem(item) || save;
+        if (save) equipment.setArmorContents(items);
+    }
+    @EventHandler public static void on(PlayerJoinEvent e) {
+        PlayerInventory inventory = e.getPlayer().getInventory();
+        ItemStack[] items = inventory.getContents();
+        boolean save = false;
+        for (ItemStack item : items) save = executeItem(item) || save;
+        if (save) inventory.setContents(items);
+    }
+}
