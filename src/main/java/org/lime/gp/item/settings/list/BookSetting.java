f90bcfb704c5ebceb2298c59e5b31459c7c96109
Lime <demn2001@gmail.com>
2023-01-23 02:48:21 +0300

    add title support

--- src/main/java/org/lime/gp/item/settings/list/BookSetting.java
+++ src/main/java/org/lime/gp/item/settings/list/BookSetting.java
@@ -13,6 +13,7 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.inventory.meta.BookMeta.Generation;
 import org.lime.gp.chat.Apply;
 import org.lime.gp.chat.ChatHelper;
+import org.lime.gp.item.BookPaper;
 import org.lime.gp.item.Items;
 import org.lime.gp.item.settings.*;
 
@@ -20,16 +21,19 @@ import com.google.common.collect.Streams;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 
+import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
 
 @Setting(name = "book") public class BookSetting extends ItemSetting<JsonObject> {
     public Optional<String> author;
+    public Optional<String> title;
     public Optional<String> generation;
     public List<List<String>> pages = new ArrayList<>();
 
     public BookSetting(Items.ItemCreator creator, JsonObject json) {
         super(creator);
         author = json.has("author") ? Optional.of(json.get("author").getAsString()) : Optional.empty();
+        title = json.has("title") ? Optional.of(json.get("title").getAsString()) : Optional.empty();
         generation = json.has("generation") ? Optional.of(json.get("generation").getAsString()) : Optional.empty();
         json.get("pages").getAsJsonArray().forEach(_json -> pages.add(_json.isJsonArray() ? Streams.stream(_json.getAsJsonArray().iterator()).map(JsonElement::getAsString).collect(Collectors.toList()) : Collections.singletonList(_json.getAsString())));
     }
@@ -38,15 +42,19 @@ import net.kyori.adventure.text.format.NamedTextColor;
         if (meta instanceof BookMeta book) {
             author.map(apply::apply).map(ChatHelper::formatComponent).ifPresent(book::author);
             generation.map(apply::apply).map(Generation::valueOf).ifPresent(book::setGeneration);
-            book.pages(pages.stream()
-                .map(v -> v.stream()
-                    .map(_v -> ChatHelper.formatText(_v, apply))
-                    .collect(Collectors.joining("\n"))
-                )
-                .flatMap(v -> Arrays.stream(v.split("\t")))
-                .map(v -> ChatHelper.formatComponent(v))
-                .map(v -> v.color(NamedTextColor.BLACK))
-                .collect(Collectors.toList()));
+            title.map(apply::apply).map(ChatHelper::formatComponent).ifPresent(book::title);
+            List<Component> pages = this.pages.stream()
+            .map(v -> v.stream()
+                .map(_v -> ChatHelper.formatText(_v, apply))
+                .collect(Collectors.joining("\n"))
+            )
+            .flatMap(v -> Arrays.stream(v.split("\t")))
+            .map(v -> ChatHelper.formatComponent(v))
+            .map(v -> v.color(NamedTextColor.BLACK))
+            .collect(Collectors.toList());
+            book.pages(pages);
+            BookPaper.setPageCount(book, pages.size());
+            BookPaper.updateInfo(book);
         }
     }
 }
