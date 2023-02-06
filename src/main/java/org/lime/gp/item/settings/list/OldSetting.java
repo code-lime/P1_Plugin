a1e9da09045a31b73b04df1d12c221c4604335b6
Lime <demn2001@gmail.com>
2023-02-06 03:20:14 +0300

    add `old` template setting

---
+++ src/main/java/org/lime/gp/item/settings/list/OldSetting.java
@@ -0,0 +1,18 @@
+package org.lime.gp.item.settings.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.lime.gp.item.Items;
+import org.lime.gp.item.settings.*;
+
+import com.google.gson.JsonArray;
+
+//TODO
+@Setting(name = "old") public class OldSetting extends ItemSetting<JsonArray> {
+    public final List<Integer> old = new ArrayList<>();
+    public OldSetting(Items.ItemCreator creator, JsonArray json) {
+        super(creator, json);
+        json.forEach(item -> old.add(item.getAsInt()));
+    }
+}
