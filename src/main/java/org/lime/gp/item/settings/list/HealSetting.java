9b8e65e856d0dd0f3e8db8df82a87f0d88e3c3ac
Lime <demn2001@gmail.com>
2023-04-22 16:41:14 +0300

    modify heal logic & add execute menu

--- src/main/java/org/lime/gp/item/settings/list/HealSetting.java
+++ src/main/java/org/lime/gp/item/settings/list/HealSetting.java
@@ -5,9 +5,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.ItemStack;
 import org.lime.system;
+import org.lime.gp.chat.Apply;
 import org.lime.gp.item.Items;
 import org.lime.gp.item.UseSetting;
 import org.lime.gp.item.settings.*;
+import org.lime.gp.player.menu.MenuCreator;
 import org.lime.gp.player.module.Death;
 
 import com.google.gson.JsonObject;
@@ -29,9 +31,14 @@ import com.google.gson.JsonObject;
     @Override public EquipmentSlot arm() { return EquipmentSlot.HAND; }
     @Override public int getTime() { return time; }
     @Override public void timeUse(Player player, Player target, ItemStack item) {
-        if (up) Death.up(target);
+        if (up) {
+            Death.up(target);
+            MenuCreator.show(player, "phone.user.die.medic_up", Apply.of().add("other_uuid", target.getUniqueId().toString()));
+        }
         double total = target.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue();
         double health = heal.getValue(total);
-        target.setHealth(Math.max(0, Math.min(this.total == null ? total : Math.min(this.total, total), target.getHealth() + health)));
+        double hp = target.getHealth();
+        double appendHeal = Math.min(hp + health, this.total == null ? total : this.total);
+        target.setHealth(Math.max(hp, appendHeal));
     }
 }
