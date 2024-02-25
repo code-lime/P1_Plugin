d53a96c6251697b24a309b4d1ca3c60c37ddd75e
Lime <demn2001@gmail.com>
2024-02-25 05:35:47 +0300

    fix

--- src/main/java/org/lime/gp/item/settings/use/target/BlockTarget.java
+++ src/main/java/org/lime/gp/item/settings/use/target/BlockTarget.java
@@ -3,26 +3,32 @@ package org.lime.gp.item.settings.use.target;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
+import org.bukkit.block.data.BlockData;
 
 import java.util.Optional;
 
 public class BlockTarget implements ITarget {
     private final Location location;
-    private final BlockState target;
+    private final BlockState state;
+    private final BlockData data;
 
-    public BlockTarget(Block target) {
-        this.location = target.getLocation();
-        this.target = target.getState();
+    public BlockTarget(Block block) {
+        this.location = block.getLocation();
+        this.data = block.getBlockData();
+        this.state = block.getState();
     }
 
     @Override public boolean isSelf() { return false; }
-    @Override public boolean isActive() { return location.getBlock().getState().equals(target); }
+    @Override public boolean isActive() { return location.getBlock().getState().equals(state); }
 
     public Location getLocation() {
         return location;
     }
     public BlockState getState() {
-        return target;
+        return state;
+    }
+    public BlockData getData() {
+        return data;
     }
 
     @Override public Optional<BlockTarget> castToBlock() { return Optional.of(this); }
