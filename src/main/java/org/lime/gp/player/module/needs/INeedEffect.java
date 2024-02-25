06a34dcc2dd067ab27bb873f7ff0db57d567cc64
Lime <demn2001@gmail.com>
2024-02-25 05:39:57 +0300

    add `text` need

--- src/main/java/org/lime/gp/player/module/needs/INeedEffect.java
+++ src/main/java/org/lime/gp/player/module/needs/INeedEffect.java
@@ -27,6 +27,7 @@ public interface INeedEffect<T extends INeedEffect<T>> {
         public static final Type<Mutate> FOOD = new Type<>(Mutate.class, Mutate::parse);
 
         public static final Type<Effect> EFFECT = new Type<>(Effect.class, Effect::parse);
+        public static final Type<Text> TEXT = new Type<>(Text.class, Text::parse);
 
         private final Class<T> tClass;
         private final Func2<Type<T>, JsonObject, T> ctor;
@@ -91,6 +92,60 @@ public interface INeedEffect<T extends INeedEffect<T>> {
             );
         }
     }
+    interface Text extends INeedEffect<Text> {
+        String message();
+        @Nullable Integer size();
+        int offset();
+        boolean shadow();
+        @Nullable TextColor color();
+
+        CustomUI.IType show();
+
+        @Override default void tick(Player player) {
+            String message = message();
+            Integer size = size();
+            if (size == null) size = ChatHelper.getTextSize(player, message);
+            ImageBuilder image = ImageBuilder.of(message, size, shadow()).withOffset(offset());
+            TextColor color = color();
+            if (color != null) image = image.withColor(color);
+
+            switch (show()) {
+                case ACTIONBAR -> CustomUI.TextUI.show(player, image);
+                case TITLE -> CustomUI.TitleUI.show(player, image);
+                case BOSSBAR -> CustomUI.BossBarUI.show(player, image);
+            }
+        }
+
+        static Text parse(Type<Text> type, JsonObject json) {
+            String message = json.get("message").getAsString();
+            Integer size = json.has("size") ? (Integer)json.get("size").getAsInt() : null;
+            int offset = json.has("offset") ? json.get("offset").getAsInt() : 0;
+            boolean shadow = !json.has("shadow") || json.get("shadow").getAsBoolean();
+            TextColor color = json.has("color") ? TextColor.fromHexString(json.get("color").getAsString()) : null;
+            CustomUI.IType show = CustomUI.IType.valueOf(json.get("show").getAsString());
+
+            return new Text() {
+                @Override public Type<Text> type() { return Type.TEXT; }
+                @Override public String message() { return message; }
+                @Override public @Nullable Integer size() { return size; }
+                @Override public int offset() { return offset; }
+                @Override public boolean shadow() { return shadow; }
+                @Override public @Nullable TextColor color() { return color; }
+                @Override public CustomUI.IType show() { return show; }
+            };
+        }
+        static IJElement docs(String type, String info) {
+            return JObject.of(
+                    JProperty.require(IName.raw("type"), IJElement.raw(type), IComment.text(info)),
+                    JProperty.require(IName.raw("message"), IJElement.text("MESSAGE"), IComment.text("Сообщение, которе будет отображено")),
+                    JProperty.optional(IName.raw("size"), IJElement.raw(16), IComment.text("Ширина сообщения в пикселях (для отображения изображений)")),
+                    JProperty.optional(IName.raw("offset"), IJElement.raw(16), IComment.text("Сдвиг сообщения по горизонтали в пикселях")),
+                    JProperty.optional(IName.raw("shadow"), IJElement.bool(), IComment.text("Отображение тени сообщения")),
+                    JProperty.optional(IName.raw("color"), IJElement.raw("#FFFFFF"), IComment.text("Hex цвет сообщения")),
+                    JProperty.require(IName.raw("show"), IJElement.or(Arrays.stream(CustomUI.IType.values()).map(Enum::name).map(IJElement::text).toList()), IComment.text("Место отображения сообщения"))
+            );
+        }
+    }
     Type<T> type();
     void tick(Player player);
 
@@ -109,7 +164,8 @@ public interface INeedEffect<T extends INeedEffect<T>> {
                 Mutate.docs("sleep", "Уменьшение времени сна"),
                 Mutate.docs("thirst", "Ускорение траты жидкости"),
                 Mutate.docs("food", "Ускорение траты еди"),
-                Effect.docs("effect", "Выдача эффекта", docs.potionEffect())
+                Effect.docs("effect", "Выдача эффекта", docs.potionEffect()),
+                Text.docs("text", "Отображение текста")
         ));
     }
 }
